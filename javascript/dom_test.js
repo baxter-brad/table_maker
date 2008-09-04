a364021cfe2e4c9234c0f1196b38739e173550d6
Brad Baxter <bmb@mail.libs.uga.edu>
2008-09-04 20:23:26 +0000

    moved javascript/css to external files

---
+++ javascript/dom_test.js
@@ -0,0 +1,9 @@
+function include( jsfile ) {
+    document.write('<script type="text/javascript" src="/javascript/'+jsfile+'"></script>'); 
+}
+include( 'add_onload.js' );
+include( 'prop_clear.js' );
+include( 'resize_page.js' );
+include( 'table_maker.js' );
+include( 'show_dom.js' );
+
